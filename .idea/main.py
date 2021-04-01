86bd16d68f0d56be1875daf3c046f33fdcf2ce16
zulker01 <zulker02@gmail.com>
2021-04-01 17:08:45 +0600

    teachers file read complete

--- .idea/main.py
+++ .idea/main.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from readWriteExcel import *
 
-
+#variables
 courseList = []
 courseCredit = {}
+highestCourseChoiceForTeachers = 5
+courseChoice = []
 
-
-courses = pd.read_excel("Courses.xlsx")
+#open courses
+courses = pd.read_excel("Routine.xlsx",sheet_name='Courses')
 print(type(courses))
 total_courses = totalRowNum(courses)
 print(total_courses)
@@ -15,3 +17,21 @@ courseList = getCourseList(courses)
 print(courseList)
 courseCredit = getCourseCredit(courses)
 print(courseCredit)
+
+#open teachers
+teachers = pd.read_excel("Routine.xlsx",sheet_name='Teachers')
+total_teachers = totalRowNum(teachers)
+courseChoice = []
+print(total_teachers)
+i = 0
+print(teachers)
+print(teachers[5][0])
+
+
+
+for i in range(total_teachers):
+    courseChoice.append(getCourseChoiceFor(teachers,i))
+    print(courseChoice[i])
+    #print(i)
+    print(getCourseChoiceFor(teachers,i))
+
--- .idea/readWriteExcel.py
+++ .idea/readWriteExcel.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
-
+#testing function
 def test():
     print("stfu")
 
 def readExcel(name):
     courses = pd.read_excel(name)
 
+#total row number from excel
 def totalRowNum(file):
     glen = file.index
     total_courses = len(glen)
     return total_courses
 
+#getting course list from Courses sheet
 def getCourseList(file):
     courseList = []
     total_course = totalRowNum(file)
@@ -20,10 +22,21 @@ def getCourseList(file):
         courseList.append(file['Course Code'][i])
     return courseList
 
+#getting course credit from Courses Sheet
 def getCourseCredit(file):
     courseList = {}
     total_course = totalRowNum(file)
     i=0
     for i in range(total_course):
         courseList[file['Course Code'][i]] = file['credit'][i]
+    return courseList
+
+
+def getCourseChoiceFor(file,i):
+    courseList = []
+    totat_teachers = totalRowNum(file)
+    j=1
+    for j in range(1,5):
+        #print(file[j][i])
+        courseList.append(file[j][i])
     return courseList
